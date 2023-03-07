6ac09e556e6153673d963c74d133d5bb009a360c
Arunabh-P <arunabh1995@gmail.com>
2023-03-07 13:18:06 +0530

    order : backend

--- server/routes/orderRoute.js
+++ server/routes/orderRoute.js
@@ -1,5 +1,10 @@
 import express from 'express';
+import { createOrder, getOrders } from '../controllers/orderController.js';
+import { verifyToken } from '../middleware/jwt.js';
 
 const router = express.Router();
+
+router.post('/:gigId', verifyToken, createOrder);
+router.get('/', verifyToken, getOrders);
 
 export default router;
