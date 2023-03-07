663ba6f4200c2ef4767b9ae79c67f12728ca1316
Arunabh-P <arunabh1995@gmail.com>
2023-03-07 08:35:50 +0530

    review backend completed

--- server/routes/reviewRoute.js
+++ server/routes/reviewRoute.js
@@ -1,5 +1,10 @@
 import express from 'express';
+import { createReview, getReviews } from '../controllers/reviewController.js';
+import { verifyToken } from '../middleware/jwt.js';
 
 const router = express.Router();
+
+router.post('/', verifyToken, createReview);
+router.get('/:gigId', getReviews);
 
 export default router;
