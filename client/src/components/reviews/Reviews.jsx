02b6226f6d62b84c8b709edab2fc6dc832d68ddc
Arunabh-P <arunabh1995@gmail.com>
2023-03-07 07:59:27 +0530

    gig api connected

--- client/src/components/reviews/Reviews.jsx
+++ client/src/components/reviews/Reviews.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import Review from '../review/Review';
+import './Reviews.scss';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import requestUrl from '../../utils/requestUrl';
 
-const Reviews = () => {
-  return <div>Reviews</div>;
+const Reviews = ({ gigId }) => {
+  const queryClient = useQueryClient();
+  const { isLoading, error, data } = useQuery({
+    queryKey: ['reviews'],
+    queryFn: () =>
+      requestUrl.get(`/reviews/${gigId}`).then((res) => {
+        return res.data;
+      }),
+  });
+
+  return (
+    <div className="reviews">
+      {isLoading
+        ? 'loading'
+        : error
+        ? 'something went wrong'
+        : data.map((review) => <Review key={review._id} review={review} />)}
+    </div>
+  );
 };
 
 export default Reviews;
--- client/src/pages/gig/Gig.jsx
+++ client/src/pages/gig/Gig.jsx
@@ -7,315 +7,174 @@ import {
   AiOutlineCheck,
 } from 'react-icons/ai';
 import { SlRefresh } from 'react-icons/sl';
+import requestUrl from '../../utils/requestUrl';
+import noAvatar from '../../Public/images/noavatar.jpg';
 
 import { Container } from 'react-bootstrap';
+import { useQuery } from '@tanstack/react-query';
+import { useParams } from 'react-router-dom';
+import Reviews from '../../components/reviews/Reviews';
 
 const Gig = () => {
-  const [index, setIndex] = useState(0);
+  const { id } = useParams();
 
+  // Carousel
+  const [index, setIndex] = useState(0);
   const handleSelect = (selectedIndex, e) => {
     setIndex(selectedIndex);
   };
+
+  const { isLoading, error, data } = useQuery({
+    queryKey: ['gig'],
+    queryFn: () =>
+      requestUrl.get(`/gigs/single/${id}`).then((res) => {
+        return res.data;
+      }),
+  });
+  const {
+    isLoading: isLoadingUser,
+    error: errorUser,
+    data: dataUser,
+  } = useQuery({
+    queryKey: ['user'],
+    queryFn: () =>
+      requestUrl.get(`/users/${data.userId}`).then((res) => {
+        return res.data;
+      }),
+  });
+
   return (
     <div className="gig">
-      <Container className="container-div">
-        <div className="left">
-          <h1 className="headline mt-4">
-            I will create ai generated art for you
-          </h1>
-          <div className="user">
-            <img
-              className="pp"
-              src="https://images.pexels.com/photos/720327/pexels-photo-720327.jpeg?auto=compress&cs=tinysrgb&w=1600"
-              alt=""
-            />
-            <span>Anna Bell</span>
-            <div className="stars">
-              <AiFillStar className="starIcon" />
-              <span>5</span>
-            </div>
-          </div>
-          <>
-            <Carousel activeIndex={index} onSelect={handleSelect}>
-              <Carousel.Item>
-                <img
-                  className="d-block w-100"
-                  src="https://img.freepik.com/free-vector/hand-drawn-picasso-style-illustration_23-2149577319.jpg?w=996&t=st=1678116054~exp=1678116654~hmac=24867754ad7121e9aa677661bd559fb7628ed8fa1072cfcc671f9bd275abd9e7"
-                  alt="First slide"
-                />
-                <Carousel.Caption>
-                  <h3>First slide label</h3>
-                  <p>
-                    Nulla vitae elit libero, a pharetra augue mollis interdum.
-                  </p>
-                </Carousel.Caption>
-              </Carousel.Item>
-              <Carousel.Item>
-                <img
-                  className="d-block w-100"
-                  src="https://img.freepik.com/free-vector/hand-drawn-illustration-stop-homophobia-concept_52683-74391.jpg?w=900&t=st=1678116080~exp=1678116680~hmac=d04ea63f0cb495a782b9b1731b4babf38427a4b6eda581bdd68fcf83b67d7294"
-                  alt="Second slide"
-                />
+      {isLoading ? (
+        'loading'
+      ) : error ? (
+        'something went wrong'
+      ) : (
+        <Container className="container-div">
+          <div className="left">
+            <h1 className="headline mt-4">{data.title}</h1>
 
-                <Carousel.Caption>
-                  <h3>Second slide label</h3>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  </p>
-                </Carousel.Caption>
-              </Carousel.Item>
-              <Carousel.Item>
-                <img
-                  className="d-block w-100"
-                  src="https://img.freepik.com/free-vector/hand-drawn-illustration-stop-homophobia-concept_52683-74392.jpg?w=900&t=st=1678116101~exp=1678116701~hmac=6c8d20a536f0db6257d33386c2197b36af02e35270df491508629d60c690a3ae"
-                  alt="Third slide"
-                />
-
-                <Carousel.Caption>
-                  <h3>Third slide label</h3>
-                  <p>
-                    Praesent commodo cursus magna, vel scelerisque nisl
-                    consectetur.
-                  </p>
-                </Carousel.Caption>
-              </Carousel.Item>
-            </Carousel>
-          </>
-          <h2>About This Gig</h2>
-          <p>
-            I use an AI program to create images based on text prompts. This
-            means I can help you to create a vision you have through a textual
-            description of your scene without requiring any reference images.
-            Some things Ive found it often excels at are: Character portraits
-            (E.g. a picture to go with your DnD character) Landscapes (E.g.
-            wallpapers, illustrations to compliment a story) Logos (E.g. Esports
-            team, business, profile picture) You can be as vague or as
-            descriptive as you want. Being more vague will allow the AI to be
-            more creative which can sometimes result in some amazing images. You
-            can also be incredibly precise if you have a clear image of what you
-            want in mind. All of the images I create are original and will be
-            found nowhere else. If you have any questions youre more than
-            welcome to send me a message.
-          </p>
-          <div className="seller">
-            <h2>About The Seller</h2>
-            <div className="user">
-              <img
-                src="https://images.pexels.com/photos/720327/pexels-photo-720327.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                alt=""
-              />
-              <div className="info">
-                <span>Anna Bell</span>
-                <div className="stars">
-                  <AiFillStar className="starIcon" />
-                  <span>5</span>
-                </div>
-              </div>
-              <button>Contact Me</button>
-            </div>
-            <div className="box">
-              <div className="items">
-                <div className="item">
-                  <span className="title">From</span>
-                  <span className="desc">USA</span>
-                </div>
-                <div className="item">
-                  <span className="title">Member since</span>
-                  <span className="desc">Aug 2022</span>
-                </div>
-                <div className="item">
-                  <span className="title">Avg. response time</span>
-                  <span className="desc">4 hours</span>
-                </div>
-                <div className="item">
-                  <span className="title">Last delivery</span>
-                  <span className="desc">1 day</span>
-                </div>
-                <div className="item">
-                  <span className="title">Languages</span>
-                  <span className="desc">English</span>
-                </div>
-              </div>
-              <hr />
-              <p>
-                My name is Anna, I enjoy creating AI generated art in my spare
-                time. I have a lot of experience using the AI program and that
-                means I know what to prompt the AI with to get a great and
-                incredibly detailed result.
-              </p>
-            </div>
-          </div>
-          <div className="reviews">
-            <h2>Reviews</h2>
-            <div className="item">
+            {isLoadingUser ? (
+              'loading'
+            ) : errorUser ? (
+              'something wentwrong'
+            ) : (
               <div className="user">
-                <img
-                  className="pp"
-                  src="https://images.pexels.com/photos/839586/pexels-photo-839586.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                  alt=""
-                />
-                <div className="info">
-                  <span>Garner David</span>
-                  <div className="country">
+                <img className="pp" src={dataUser.img || noAvatar} alt="" />
+                <span>{dataUser.username}</span>
+                {!isNaN(data.totalStars / data.starNumber) && (
+                  <div className="stars">
+                    {Array(Math.round(data.totalStars / data.starNumber))
+                      .fill()
+                      .map((item, i) => (
+                        <AiFillStar className="starIcon" key={i} />
+                      ))}
+                    <span>
+                      {' '}
+                      {Math.round(data.totalStars / data.starNumber)}
+                    </span>
+                  </div>
+                )}
+              </div>
+            )}
+            <>
+              <Carousel activeIndex={index} onSelect={handleSelect}>
+                {data.images.map((img) => (
+                  <Carousel.Item key={img}>
                     <img
-                      src="https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1fa-1f1f8.png"
-                      alt=""
+                      className="d-block w-100"
+                      src={img}
+                      alt="First slide"
                     />
-                    <span>United States</span>
+                  </Carousel.Item>
+                ))}
+              </Carousel>
+            </>
+            <h2>About This Gig</h2>
+            <p>{data.desc}</p>
+            {isLoadingUser ? (
+              'loading'
+            ) : errorUser ? (
+              'something wentwrong'
+            ) : (
+              <div className="seller">
+                <h2>About The Seller</h2>
+                <div className="user">
+                  <img src={dataUser.img || noAvatar} alt="" />
+                  <div className="info">
+                    <span>{dataUser.username}</span>
+                    {!isNaN(data.totalStars / data.starNumber) && (
+                      <div className="stars">
+                        {Array(Math.round(data.totalStars / data.starNumber))
+                          .fill()
+                          .map((item, i) => (
+                            <AiFillStar className="starIcon" key={i} />
+                          ))}
+                        <span>
+                          {' '}
+                          {Math.round(data.totalStars / data.starNumber)}
+                        </span>
+                      </div>
+                    )}
                   </div>
                 </div>
-              </div>
-              <div className="stars">
-                <img src="/img/star.png" alt="" />
-                <img src="/img/star.png" alt="" />
-                <img src="/img/star.png" alt="" />
-                <img src="/img/star.png" alt="" />
-                <img src="/img/star.png" alt="" />
-                <span>5</span>
-              </div>
-              <p>
-                I just want to say that art_with_ai was the first, and after
-                this, the only artist Ill be using on Fiverr. Communication was
-                amazing, each and every day he sent me images that I was free to
-                request changes to. They listened, understood, and delivered
-                above and beyond my expectations. I absolutely recommend this
-                gig, and know already that Ill be using it again very very soon
-              </p>
-              <div className="helpful">
-                <span>Helpful?</span>
-                <img src="/img/like.png" alt="" />
-                <span>Yes</span>
-                <img src="/img/dislike.png" alt="" />
-                <span>No</span>
-              </div>
-            </div>
-            <hr />
-            <div className="item">
-              <div className="user">
-                <img
-                  className="pp"
-                  src="https://images.pexels.com/photos/4124367/pexels-photo-4124367.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                  alt=""
-                />
-                <div className="info">
-                  <span>Sidney Owen</span>
-                  <div className="country">
-                    <img
-                      src="https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1e9-1f1ea.png"
-                      alt=""
-                    />
-                    <span>Germany</span>
+                <div className="box">
+                  <div className="items">
+                    <div className="item">
+                      <span className="title">From</span>
+                      <span className="desc">{dataUser.country}</span>
+                    </div>
+                    <div className="item">
+                      <span className="title">Member since</span>
+                      <span className="desc">Aug 2022</span>
+                    </div>
+                    <div className="item">
+                      <span className="title">Avg. response time</span>
+                      <span className="desc">4 hours</span>
+                    </div>
+                    <div className="item">
+                      <span className="title">Last delivery</span>
+                      <span className="desc">1 day</span>
+                    </div>
+                    <div className="item">
+                      <span className="title">Languages</span>
+                      <span className="desc">English</span>
+                    </div>
                   </div>
+                  <hr />
+                  <p>{dataUser.desc}</p>
                 </div>
               </div>
-              <div className="stars">
-                <img src="/img/star.png" alt="" />
-                <img src="/img/star.png" alt="" />
-                <img src="/img/star.png" alt="" />
-                <img src="/img/star.png" alt="" />
-                <img src="/img/star.png" alt="" />
-                <span>5</span>
+            )}
+            <Reviews gigId={id} />
+          </div>
+
+          <div className="right mt-5">
+            <h3 className="subHead">{data.shortTitle}</h3>
+            <p className="amount">₹ {data.price}</p>
+            <p>{data.shortDesc}</p>
+            <div className="details">
+              <div className="item">
+                <AiOutlineClockCircle />
+                <span>{data.deliveryDate} Days Delivery</span>
               </div>
-              <p>
-                The designer took my photo for my book cover to the next level!
-                Professionalism and ease of working with designer along with
-                punctuality is above industry standards!! Whatever your project
-                is, you need this designer!
-              </p>
-              <div className="helpful">
-                <span>Helpful?</span>
-                <img src="/img/like.png" alt="" />
-                <span>Yes</span>
-                <img src="/img/dislike.png" alt="" />
-                <span>No</span>
+              <div className="item">
+                <SlRefresh />
+                <span>{data.revisionNumber} Revisions</span>
               </div>
             </div>
-            <hr />
-            <div className="item">
-              <div className="user">
-                <img
-                  className="pp"
-                  src="https://images.pexels.com/photos/842980/pexels-photo-842980.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                  alt=""
-                />
-                <div className="info">
-                  <span>Lyle Giles </span>
-                  <div className="country">
-                    <img
-                      src="https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1fa-1f1f8.png"
-                      alt=""
-                    />
-                    <span>United States</span>
-                  </div>
+            <div className="features">
+              {data.features.map((feature) => (
+                <div className="item" key={feature}>
+                  <AiOutlineCheck className="checkIcon" />
+                  <span>{feature}</span>
                 </div>
-              </div>
-              <div className="stars">
-                <img src="/img/star.png" alt="" />
-                <img src="/img/star.png" alt="" />
-                <img src="/img/star.png" alt="" />
-                <img src="/img/star.png" alt="" />
-                <img src="/img/star.png" alt="" />
-                <span>5</span>
-              </div>
-              <p>
-                Amazing work! Communication was amazing, each and every day he
-                sent me images that I was free to request changes to. They
-                listened, understood, and delivered above and beyond my
-                expectations. I absolutely recommend this gig, and know already
-                that Ill be using it again very very soon
-              </p>
-              <div className="helpful">
-                <span>Helpful?</span>
-                <img src="/img/like.png" alt="" />
-                <span>Yes</span>
-                <img src="/img/dislike.png" alt="" />
-                <span>No</span>
-              </div>
+              ))}
             </div>
+            <button>Continue</button>
           </div>
-        </div>
-
-        <div className="right mt-5">
-          <h3 className="subHead">1 AI generated image</h3>
-          <p className="amount">₹ 59.99</p>
-          <p>
-            I will create a unique high quality AI generated image based on a
-            description that you give me
-          </p>
-          <div className="details">
-            <div className="item">
-              <AiOutlineClockCircle />
-              <span>2 Days Delivery</span>
-            </div>
-            <div className="item">
-              <SlRefresh />
-              <span>3 Revisions</span>
-            </div>
-          </div>
-          <div className="features">
-            <div className="item">
-              <AiOutlineCheck className="checkIcon" />
-              <span>Prompt writing</span>
-            </div>
-            <div className="item">
-              <AiOutlineCheck className="checkIcon" />
-
-              <span>Artwork delivery</span>
-            </div>
-            <div className="item">
-              <AiOutlineCheck className="checkIcon" />
-
-              <span>Image upscaling</span>
-            </div>
-            <div className="item">
-              <AiOutlineCheck className="checkIcon" />
-
-              <span>Additional design</span>
-            </div>
-          </div>
-          <button>Continue</button>
-        </div>
-      </Container>
+        </Container>
+      )}
     </div>
   );
 };
