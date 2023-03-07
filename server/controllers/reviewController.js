f34c93a2ab05ab278b5f9cd7ba51035de409d2b5
Arunabh-P <arunabh1995@gmail.com>
2023-03-07 08:05:14 +0530

    review : backend begins

---
+++ server/controllers/reviewController.js
@@ -0,0 +1,8 @@
+import createError from "../utils/createError/js";
+import Review from "../models/reviewModel.js"
+import Gig from "../models/gigModel.js"
+
+// 1. create Review
+export const createReview = async(req,res,next)=>{
+    if(req.isSe)
+}
